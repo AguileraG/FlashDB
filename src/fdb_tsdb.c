5ee33168b0da6c64b1442724daf327512e985ed8
Antonio Aguilera <antonio@infitech.es>
2023-06-06 11:40:25 +0200

    Fix possible null dereference in FDB_ASSERT due to FDB_LOG_PREFIX2

--- src/fdb_tsdb.c
+++ src/fdb_tsdb.c
@@ -21,7 +21,7 @@
 #define FDB_LOG_TAG "[tsl]"
 /* rewrite log prefix */
 #undef  FDB_LOG_PREFIX2
-#define FDB_LOG_PREFIX2()                         FDB_PRINT("[%s] ", db_name(db))
+#define FDB_LOG_PREFIX2()                         FDB_PRINT("[%s] ", (db) ? db_name(db): "(null)")
 
 #if defined(FDB_USING_TSDB)
 
